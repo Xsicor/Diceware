e9c6357efd81d53e79db371144acfbb7fa094b8e
digital454 <vara2145@gmail.com>
2022-06-12 16:59:30 +0100

    minor changes

--- my-app/src/generate/GenerateContainer.js
+++ my-app/src/generate/GenerateContainer.js
@@ -8,27 +8,34 @@ function GenerateContainer(props) {
     const [word, id] = generateWordAndId();
     return {
       word: capitalizeFirstLetter(word),
-      id: id
-    }
-  })
-  const password = generatedWords.map(word => word.word).join('')
+      id: id,
+    };
+  });
+  const password = generatedWords.map((word) => word.word).join("");
 
   return (
     <div className="generateContainer">
       <div className="rollResults">
         {loopNum.map((i) => {
-          return <GeneratedWord word={generatedWords[i].word} id={generatedWords[i].id} />;
+          return (
+            <GeneratedWord
+              word={generatedWords[i].word}
+              id={generatedWords[i].id}
+            />
+          );
         })}
       </div>
-      <div className="password">
-        <h1>
-          The generated password is: <br/>
-          {password}
+      <div className="passwordContainer">
+        <h1 className="passwordText">
+          The generated password is:<br/>
         </h1>
+        <h1 className="pass">{password}</h1>
       </div>
     </div>
   );
 }
+
+export default GenerateContainer;
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
@@ -47,5 +54,3 @@ function generateWordAndId() {
 function getRandomInt(max) {
   return Math.floor(Math.random() * max) + 1;
 }
-
-export default GenerateContainer;
